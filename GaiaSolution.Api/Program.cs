4d8ff30ea395b89ef0b12c35929a06487975900d
BASTIEN <bastien.grenot@viacesi.fr>
2025-09-02 18:19:26 +0200

    Create GaiaSolution.Api

---
+++ GaiaSolution.Api/Program.cs
@@ -0,0 +1,40 @@
+using DotNetEnv;
+
+namespace GaiaSolution.Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Env.Load(); 
+        var envName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"; 
+        Env.Load($".env.{envName.ToLower()}");
+        
+        
+        
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
