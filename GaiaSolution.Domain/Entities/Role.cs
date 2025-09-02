33159f6be998b82399516e0a9829921617195553
BASTIEN <bastien.grenot@viacesi.fr>
2025-09-02 19:34:32 +0200

    Create Role.cs entities

---
+++ GaiaSolution.Domain/Entities/Role.cs
@@ -0,0 +1,18 @@
+﻿namespace GaiaSolution.Domain.Entities;
+/*
+ * Représente un rôle attribué à un utilisateur dans le système (ex: Admin, Utilisateur, etc.).
+ * Utilisé pour la gestion des permissions et de l'accès.
+ */
+
+public enum RoleEnum
+{
+    Admin,
+    Doctor,
+    Cleaning,
+    Developer,
+}
+public class Role
+{
+    public int RoleId { get; set; }
+    public RoleEnum RoleName { get; set; }
+}
